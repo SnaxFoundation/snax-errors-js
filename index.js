42df2cb75c5b95ca416bb702d22179f091c04c88
bee-warrior <casual.bee.warrior@gmail.com>
2019-05-27 22:06:13 +0300

    init

---
+++ index.js
@@ -0,0 +1,71 @@
+class SnaxError extends Error {
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      code: this.code
+    };
+  }
+}
+
+class AlreadyExists extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "AlreadyExists";
+    this.message = message;
+    this.code = 500;
+  }
+}
+
+class BadRequest extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "BadRequest";
+    this.message = message;
+    this.code = 400;
+  }
+}
+
+class NotFound extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "NotFound";
+    this.message = message;
+    this.code = 404;
+  }
+}
+
+class General extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "General";
+    this.message = message;
+    this.code = 500;
+  }
+}
+class Twitter extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "Twitter";
+    this.message = message;
+    this.code = 500;
+  }
+}
+
+class Restriction extends SnaxError {
+  constructor(message) {
+    super(message);
+    this.name = "Restriction";
+    this.message = message;
+    this.code = 500;
+  }
+}
+
+module.exports = {
+  AlreadyExists,
+  BadRequest,
+  NotFound,
+  General,
+  Twitter,
+  Restriction
+};
